e968b067b967d1c837db56c19fbbe3f92429d66d
leraloskutova <valeriia.loskutova@yandex.ru>
2020-08-14 21:09:49 +0700

    Создание http сервера и изменение структуры проекта
    1. CSS-код вынесен в файл app.component.css.
    2. HTML-код вынесен в файл app.component.css.
    3. Добавлены изображения (аватар пользователя, иконки редактирования и удаления).
    4. Реализован интерфейс таблицы.
    5. В файле http.service.ts реализован функционал получения данных с json-сервера.
    6. В файле app.component.ts происходит выгрузка данных по запросу.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService} from './http.service';
+import { Person } from './person';
 
 @Component({
     selector: 'my-app',
-    template: ``
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    providers: [HttpService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+    persons: Person[]=[];
+
+    constructor(private httpService: HttpService){}
+
+    ngOnInit(){
+
+        this.httpService.getData().subscribe(data => this.persons=data["persons"]);
+    }
 }

6ebdcee7878e2c27720a3e1deccd8c92810d8d26
leraloskutova <valeriia.loskutova@yandex.ru>
2020-08-16 01:38:00 +0700

    Реализация модальных окон и CRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,35 @@ import { Person } from './person';
 export class AppComponent implements OnInit {
 
     persons: Person[]=[];
+    person: Person = new Person();
 
     constructor(private httpService: HttpService){}
 
     ngOnInit(){
-
-        this.httpService.getData().subscribe(data => this.persons=data["persons"]);
+        this.httpService.getData().subscribe(data => this.persons=(data as Array<Person>));
     }
+
+    put(person: Person){
+        this.httpService.putData(person).subscribe((data: Person) => this.person=data);
+        window.location.href = "#";
+    }
+
+    post(person: Person){
+        this.httpService.postData(person).subscribe((data: Person) => this.person=data);
+        window.location.href = "http://localhost:4200/";
+    }
+
+    delete(person: Person){
+        this.httpService.deleteData(person.id).subscribe();
+        window.location.href = "http://localhost:4200/";
+    }
+
+    closeWindow() {
+        window.location.href = "#";
+    }
+
+    selectRow(person){
+        this.person = person;
+    }
+
 }
--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Person} from './person';
 
 @Injectable()
 export class HttpService{
@@ -7,6 +8,19 @@ export class HttpService{
     constructor(private http: HttpClient){ }
 
     getData(){
-        return this.http.get('http://localhost:3000/db')
+        return this.http.get('http://localhost:3000/api/v1/persons');
+    }
+
+    putData(person: Person){
+        return this.http.put(`http://localhost:3000/persons/${person.id}`, person);
+    }
+
+    postData(person: Person){
+        const data = {id: undefined, firstName: person.firstName, lastName: person.lastName};
+        return this.http.post('http://localhost:3000/persons', data);
+    }
+
+    deleteData(id: number) {
+        return this.http.delete(`http://localhost:3000/persons/${id}`);
     }
 }
